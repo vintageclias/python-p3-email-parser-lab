b8c9ff7a8670802651af2249702bc0b370d2a6e0
vintageclias <vintageclias@gmail.com>
2025-03-09 16:30:20 -0700

    email

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+        
+    
+    def parse(self):
+        exp = re.compile(r'[\w\.-]+@[\w\.-]+')
+        return sorted(exp.findall(self.string))
